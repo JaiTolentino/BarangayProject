111230da1e6e38fa0c1bc74d8f28dc408d4b7cb4
Harold <98804534+Kopiiii@users.noreply.github.com>
2023-04-12 18:05:35 +0000

    implemented pop-up for certificate application

---
+++ scripts/certapplied.js
@@ -0,0 +1,48 @@
+function applycert1() {
+    var x = document.getElementById("cert1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function closecert1() {
+    var x = document.getElementById("cert1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function applycert2() {
+    var x = document.getElementById("cert2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function closecert2() {
+    var x = document.getElementById("cert2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function applycert3() {
+    var x = document.getElementById("cert3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function closecert3() {
+    var x = document.getElementById("cert3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
